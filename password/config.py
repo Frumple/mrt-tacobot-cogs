24f876d5c776421514015b9373585c2b9aae28c7
Frumple <frumple1@gmail.com>
2023-11-09 15:27:31 -0800

    Password cog: Add commands to set button style and emoji, and to list all services.

--- password/config.py
+++ password/config.py
@@ -1,17 +1,20 @@
-from discord import ButtonStyle, Interaction, TextChannel
-from discord.errors import NotFound
+from discord import ButtonStyle, Interaction, PartialEmoji, TextChannel
+from discord.errors import HTTPException, NotFound
 from discord.ui import Button, View
 from redbot.core import Config, app_commands, commands, checks
 from redbot.core.bot import Red
+from redbot.core.utils.chat_formatting import humanize_list
 
 class PasswordButton(Button):
   def __init__(self, service_name: str, service: dict, log_channel: TextChannel = None, response_lifespan: int = None):
     # Setting the custom_id to a unique value ensures the view is persistent
     super().__init__(
-      style = ButtonStyle.primary,
-      label = service['button_text'],
       custom_id = service_name
     )
+    self.label = service['button_text']
+    self.style = getattr(ButtonStyle, service['style']) if 'style' in service and service['style'] is not None else ButtonStyle.primary
+    self.emoji = PartialEmoji.from_str(service['emoji']) if 'emoji' in service and service['emoji'] is not None else None
+
     self.service_name = service_name
     self.service = service
     self.log_channel = log_channel
@@ -67,7 +70,7 @@ class PasswordConfig:
   async def password_config_set_message_channel(self, ctx: commands.Context, channel: TextChannel) -> None:
     """Sets the channel that contains the message."""
     await self.config.message_channel_id.set(channel.id)
-    await ctx.send(f'Message channel has been set to: {channel.mention}')
+    await ctx.send(f'Password message channel has been set to: {channel.mention}')
     await self.password_config_update(ctx)
 
   @password_config.command(name='set_message_text')
@@ -77,7 +80,7 @@ class PasswordConfig:
   async def password_config_set_message_text(self, ctx: commands.Context, *, message_text: str) -> None:
     """Sets the text of the message."""
     await self.config.message_text.set(message_text)
-    await ctx.send(f'Message text has been set to: {message_text}')
+    await ctx.send(f'Password message text has been set to: {message_text}')
     await self.password_config_update(ctx)
 
   @password_config.command(name='set_log_channel')
@@ -85,13 +88,13 @@ class PasswordConfig:
   @app_commands.default_permissions(administrator=True)
   @app_commands.checks.has_permissions(administrator=True)
   async def password_config_set_log_channel(self, ctx: commands.Context, channel: TextChannel = None) -> None:
-    """Sets the channel that logs all password retrievals (optional)."""
+    """Sets the optional channel that logs all password retrievals."""
     if channel is None:
       await self.config.log_channel_id.clear()
-      await ctx.send('Log channel is now disabled.')
+      await ctx.send('Password log channel is now disabled.')
     else:
       await self.config.log_channel_id.set(channel.id)
-      await ctx.send(f'Log channel has been set to: {channel.mention}')
+      await ctx.send(f'Password log channel has been set to: {channel.mention}')
     await self.password_config_update(ctx)
 
   @password_config.command(name='set_response_lifespan')
@@ -108,6 +111,28 @@ class PasswordConfig:
       await ctx.send(f'Response lifespan has been set to: {lifespan} seconds')
     await self.password_config_update(ctx)
 
+  @password_config.command(name='list_services')
+  @checks.admin_or_permissions()
+  @app_commands.default_permissions(administrator=True)
+  @app_commands.checks.has_permissions(administrator=True)
+  async def password_config_list_services(self, ctx: commands.Context) -> None:
+    """Lists all services."""
+    async with self.config.services() as services:
+      for key, value in services.items():
+        button_text = value['button_text']
+        description = value['description']
+        password = value['password']
+        style = value['style'] if 'style' in value else None
+        emoji = value['emoji'] if 'emoji' in value else None
+
+        text = f'**Service: `{key}`**\n'
+        text += f'- Button text: `{button_text}`\n'
+        text += f'- Description: `{description}`\n'
+        text += f'- Password: `{password}`\n'
+        text += f'- Style: `{style}`\n'
+        text += f'- Emoji: {emoji}\n'
+        await ctx.send(text)
+
   @password_config.command(name="add_service")
   @checks.admin_or_permissions()
   @app_commands.default_permissions(administrator=True)
@@ -120,7 +145,9 @@ class PasswordConfig:
         services[service_name] = {
           'button_text': service_name,
           'description': 'Password',
-          'password': ""
+          'password': "",
+          'style': 'primary',
+          'emoji': None,
         }
         await ctx.send(f'Service `{service_name}` added.')
         success = True
@@ -155,11 +182,10 @@ class PasswordConfig:
     success = False
     async with self.config.services() as services:
       if service_name in services:
-        services[service_name] = {
-          'button_text': button_text,
-          'description': services[service_name]['description'],
-          'password': services[service_name]['password']
-        }
+        service = services[service_name]
+        service['button_text'] = button_text
+        services[service_name] = service
+
         await ctx.send(f'Button text for service `{service_name}` set to: {button_text}.')
         success = True
       else:
@@ -176,13 +202,70 @@ class PasswordConfig:
     success = False
     async with self.config.services() as services:
       if service_name in services:
-        services[service_name] = {
-          'button_text': services[service_name]['button_text'],
-          'description': description,
-          'password': services[service_name]['password']
-        }
+        service = services[service_name]
+        service['description'] = description
+        services[service_name] = service
+
         await ctx.send(f'Description for service `{service_name}` set to: {description}.')
         success = True
+      else:
+        await ctx.send(f'Service `{service_name}` does not exist.')
+    if success:
+      await self.password_config_update(ctx)
+
+  @password_config.command(name="set_style")
+  @checks.admin_or_permissions()
+  @app_commands.default_permissions(administrator=True)
+  @app_commands.checks.has_permissions(administrator=True)
+  async def password_config_set_style(self, ctx: commands.Context, service_name: str, *, style: str) -> None:
+    """Sets the button style for the given service."""
+    success = False
+    async with self.config.services() as services:
+      if service_name in services:
+        valid_styles = [
+            i for i in dir(ButtonStyle) if not i.startswith("_") and i != "try_value"
+        ]
+
+        if style.lower() in valid_styles:
+          service = services[service_name]
+          service['style'] = style
+          services[service_name] = service
+
+          await ctx.send(f'Style for service `{service_name}` set to: {style}.')
+          success = True
+        else:
+          style_list = humanize_list(valid_styles)
+          await ctx.send(f'Error: `{style}` is not a valid style. Choose from: `{style_list}`')
+      else:
+        await ctx.send(f'Service `{service_name}` does not exist.')
+    if success:
+      await self.password_config_update(ctx)
+
+  @password_config.command(name="set_emoji")
+  @checks.admin_or_permissions()
+  @app_commands.default_permissions(administrator=True)
+  @app_commands.checks.has_permissions(administrator=True)
+  async def password_config_set_emoji(self, ctx: commands.Context, service_name: str, *, emoji: str = None) -> None:
+    """Sets the optional emoji for the given service."""
+    success = False
+    async with self.config.services() as services:
+      if service_name in services:
+        service = services[service_name]
+
+        try:
+          if emoji is None:
+            service['emoji'] = None
+            await ctx.send(f'Emoji for service `{service_name}` cleared.')
+          else:
+            # Check that this is a valid emoji, throw exception if it is not
+            PartialEmoji.from_str(emoji)
+            service['emoji'] = emoji
+            await ctx.send(f'Emoji for service `{service_name}` set to: {emoji}.')
+
+          services[service_name] = service
+          success = True
+        except HTTPException:
+          await ctx.send(f'Error: {emoji} is not a valid emoji.')
       else:
         await ctx.send(f'Service `{service_name}` does not exist.')
     if success:
@@ -197,11 +280,10 @@ class PasswordConfig:
     success = False
     async with self.config.services() as services:
       if service_name in services:
-        services[service_name] = {
-          'button_text': services[service_name]['button_text'],
-          'description': services[service_name]['description'],
-          'password': password
-        }
+        service = services[service_name]
+        service['password'] = password
+        services[service_name] = service
+
         await ctx.send(f'Password for service `{service_name}` set.')
         success = True
       else:
@@ -222,7 +304,7 @@ class PasswordConfig:
     response_lifespan = await self.config.response_lifespan()
 
     if message_channel_id is None:
-      await ctx.send('Message channel has not been set. Run `[p]password_config set_message_channel` to set it.')
+      await ctx.send('Password message channel has not been set. Run `[p]password_config set_message_channel` to set it.')
       return
 
     log_channel: TextChannel = None
@@ -244,13 +326,13 @@ class PasswordConfig:
 
     # If the button message does not exist in Discord, create a new message
     except NotFound:
-      await ctx.send(f'Message does not exist, recreating...')
+      await ctx.send(f'Password message does not exist, recreating...')
       await self.create_button_message(ctx, message_channel, view, message_text)
       return
 
     # Otherwise, edit the existing message
     await message.edit(content = message_text, view = view)
-    await ctx.send(f'Message updated: {message.jump_url}.', suppress_embeds = True)
+    await ctx.send(f'Password message updated: {message.jump_url}.', suppress_embeds = True)
 
   async def create_button_message(self, ctx: commands.Context, channel: TextChannel, view: View, message_text: str):
     message = await channel.send(message_text, view = view)
